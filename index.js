af7ebf804b88d5324f3d7c336786c20d55fb89a4
Evgeniy-the-Great <shalenkoevgeniy@gmail.com>
2021-09-02 17:33:15 +0300

    add updates

--- index.js
+++ index.js
@@ -5,27 +5,45 @@ const refs = {
   secs: document.querySelector('[data-value="secs"]'),
 };
 
-const targetDate = new Date('2022-01-22').getTime();
+const targetDate = new Date('2021-08-03').getTime();
 
 function num(i) {
   return String(i).padStart(2, 0);
 }
 
+let selector = null;
 
-function timeLeft() {
-  const now = Date.now();
-  const time = targetDate - now;
 
+class CountdownTimer {
+  timeLeft() {
+    selector = setInterval(() => {
+      const now = Date.now();
+      const deltaTime = targetDate - now;
+      const { days, hours, mins, secs } = getTimeComponents(deltaTime);
+  
+      refs.days.textContent = days;
+      refs.hours.textContent = num(hours);
+      refs.mins.textContent = num(mins);
+      refs.secs.textContent = num(secs);
+    }, 1000);
+    
+    if (targetDate <= Date.now()) {
+      clearInterval(selector);
+      refs.days.textContent = '00';
+      refs.hours.textContent = '00';
+      refs.mins.textContent = '00';
+      refs.secs.textContent = '00';
+    }
+  }
+}
+
+const timer = new CountdownTimer();
+// const timeLeft();
+
+function getTimeComponents(time) {
   const days = Math.floor(time / (1000 * 60 * 60 * 24));
   const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   const secs = Math.floor((time % (1000 * 60)) / 1000);
-
-  refs.days.textContent = days;
-  refs.hours.textContent = num(hours);
-  refs.mins.textContent = num(mins);
-  refs.secs.textContent = num(secs);
+  return { days, hours, mins, secs };
 }
-timeLeft();
-
-const id = setInterval(timeLeft, 1000);
